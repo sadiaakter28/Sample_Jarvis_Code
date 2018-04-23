89e9c1c7359ab6755803cbe9443fd1a7aef19532
Sadia Akter <34855723+sadiaakter28@users.noreply.github.com>
2018-04-24 00:01:18 +0600

    First commit

---
+++ simple_jarvis full Program/simple_jarvis/simple_jarvis/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace simple_jarvis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape2 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape3 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.ovalShape3,
+            this.ovalShape2,
+            this.ovalShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(584, 467);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // ovalShape1
+            // 
+            this.ovalShape1.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.ovalShape1.BorderWidth = 10;
+            this.ovalShape1.Location = new System.Drawing.Point(17, 53);
+            this.ovalShape1.Name = "ovalShape1";
+            this.ovalShape1.Size = new System.Drawing.Size(102, 101);
+            // 
+            // ovalShape2
+            // 
+            this.ovalShape2.BorderColor = System.Drawing.Color.Blue;
+            this.ovalShape2.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.ovalShape2.BorderWidth = 10;
+            this.ovalShape2.Location = new System.Drawing.Point(34, 71);
+            this.ovalShape2.Name = "ovalShape2";
+            this.ovalShape2.Size = new System.Drawing.Size(68, 65);
+            // 
+            // ovalShape3
+            // 
+            this.ovalShape3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ovalShape3.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape3.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.ovalShape3.Location = new System.Drawing.Point(54, 89);
+            this.ovalShape3.Name = "ovalShape3";
+            this.ovalShape3.Size = new System.Drawing.Size(30, 29);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 300;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 300;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 300;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(584, 467);
+            this.Controls.Add(this.shapeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape3;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ simple_jarvis full Program/simple_jarvis/simple_jarvis/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace simple_jarvis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x = 1;
+
+        SpeechRecognitionEngine mRecognize = new SpeechRecognitionEngine();
+
+        public void mRecog_aktif()
+        {
+            try
+            {
+                Choices sList = new Choices();
+
+                sList.Add(new string[] { "open google","open my facebook","open my folder" });
+
+                Grammar gr = new Grammar(new GrammarBuilder(sList));
+                mRecognize.RequestRecognizerUpdate();
+                mRecognize.LoadGrammar(gr);
+                mRecognize.SpeechRecognized += mRecognize_SpeechRecognized;
+                mRecognize.SetInputToDefaultAudioDevice();
+                mRecognize.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+
+
+
+        void mRecognize_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text == "open google")
+            {
+                System.Diagnostics.Process.Start("www.google.com");
+            }
+
+            else if (e.Result.Text == "open my facebook")
+            {
+                System.Diagnostics.Process.Start("www.facebook.com");
+            }
+
+            else if (e.Result.Text == "open my folder")
+            {
+                System.Diagnostics.Process.Start("c:\\");
+            }
+
+        }
+
+
+        #region animasi
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+                ovalShape2.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                timer1.Stop();
+                timer2.Start();
+            
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+          
+                ovalShape2.BorderStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+                timer2.Stop();
+                timer3.Start();
+            
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+           
+                ovalShape2.BorderStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+                timer3.Stop();
+                timer4.Start();
+            
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+           
+                ovalShape2.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                timer4.Stop();
+                timer1.Start();
+          
+        }
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mRecog_aktif();
+        }
+
+    }
+}
